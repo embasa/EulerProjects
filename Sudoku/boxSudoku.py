17d89975cacd92d4fd23f0ee3cb7aadc18fda873
bruno hernandez <embasa@users.noreply.github.com>
2015-04-06 15:09:43 -0700

    Delete boxSudoku.py

--- Sudoku/boxSudoku.py
+++
@@ -1,9 +0,0 @@
-from pyeda.inter import *
-size = 3
-box = [[[False for i in range(size)]for j in range(size)]for k in range(size)]
-
-
-for k in range(size):
-    for j in range(size):
-        print box[k][j], '\t',
-    print ''
