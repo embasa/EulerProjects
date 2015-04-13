73240606096c70575bf409dfb3811b0a95f76053
embasa <embasa@jeff.cis.cabrillo.edu>
2015-04-13 01:54:51 -0700

    adding prime generator

---
+++ primeGen.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class primeGen{
+  public static int[] getPrimes(int range){
+    boolean[] nums = new boolean[range +1];
+    Arrays.fill(nums,true);
+    for(int i=0;i<nums.length;i++){
+
+    }
+    return null;
+  }
+
+  public static void main(String[] args){
+    int size = 1000000;
+    primeGen.getPrimes(size);    
+  }
+}
